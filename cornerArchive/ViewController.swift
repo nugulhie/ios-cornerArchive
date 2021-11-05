091f328d3eb01a9bc601507b9e34214fc51b2afe
황혁주 <nugulhie@gmail.com>
2021-11-05 19:01:57 +0900

    #2 Dev UI splash view, main login view

--- cornerArchive/ViewController.swift
+++ cornerArchive/ViewController.swift
@@ -6,14 +6,17 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+ 
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
+    
 
 }
 
---
+++ cornerArchive/ViewControllers/LoginController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginController.swift
+//  cornerArchive
+//
+//  Created by 황혁주 on 2021/11/05.
+//
+
+import UIKit
+
+class LoginController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
